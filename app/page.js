376570174bc75166fceabf260779a1cb86422c37
Daniel Rojas <danielrojas243@gmail.com>
2023-09-16 20:05:14 -0500

    feat(componente-useVelocity) crear comp. parallax

--- app/page.js
+++ app/page.js
@@ -6,8 +6,14 @@ import {
   Cards,
   ImageTextBlock,
   Carousel,
-  Footer
+  Footer,
 } from "./components";
+
+import iconMobile from "/public/icon-mobile.svg"
+import iconLightning from "/public/icon-lightning.svg"
+import iconFrontEnd from "/public/icon-front-end.svg"
+import iconDesktop from "/public/icon-desktop.svg"
+import iconCode from "/public/icon-code.svg"
 
 export default function Home() {
   return (
@@ -15,12 +21,18 @@ export default function Home() {
       <Header />
       <main>
         <HeroTextImage />
-        <Brands />
+        <Brands baseVelocity={-1}>
+          <p><Image src={iconMobile} width={18} height={18} alt="iconMobile" />Desktop</p>
+          <p><Image src={iconLightning} width={15} height={15} alt="iconLightning" />Front end</p>
+          <p><Image src={iconFrontEnd} width={28} height={28} alt="iconFrontEnd" />Development</p>
+          <p><Image src={iconDesktop} width={23} height={23} alt="iconDesktop" />Mobile</p>
+          <p><Image src={iconCode} width={25} height={25} alt="iconCode" />Frameworks</p>
+        </Brands>
         <Cards />
         <ImageTextBlock />
         <Carousel />
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
---
+++ app/utils/motion.js
@@ -0,0 +1,7 @@
+export const fadeVariants = {
+  initial: { opacity: 0, y: -15 },
+  visible: {
+    opacity: 1,
+    y: 0
+  },
+};
